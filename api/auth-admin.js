626e48e44ce4d14370eba508e52c6b0d25168e3e
tanjin-system <fishsky99@gmail.com>
2026-09-09 18:19:50 +0800

    Update auth-admin.js

--- api/auth-admin.js
+++ api/auth-admin.js
@@ -42,7 +42,7 @@ module.exports = async (req, res) => {
 
   const token = jwt.sign(
     { role: 'app_admin' },
-    process.env.SUPABASE_JWT_SECRET,
+   'tanjin_secret_key_2026',
     { expiresIn: '12h' }
   );
 
